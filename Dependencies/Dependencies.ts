66ec5de30ad7ae598d274ebeb6e24617266b7c67
Luis Salamanca <luisguezguann@gmail.com>
2025-06-10 06:52:22 -0500

    Agrego la conexion

--- Dependencies/Dependencies.ts
+++ Dependencies/Dependencies.ts
@@ -2,4 +2,6 @@ export { Application, Router, Context, send } from 'https://deno.land/x/oak@v17.
 export { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts";
 export { z } from "https://deno.land/x/zod@v3.24.1/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.203.0/fs/mod.ts";
+export {create,verify,decode,getNumericDate} from "https://deno.land/x/djwt@v3.0.2/mod.ts" 
+export {encodeBase64Url} from "https://deno.land/std@0.224.0/encoding/base64url.ts";
+export {ensureDir} from "https://deno.land/std@0.208.0/fs/mod.ts";
